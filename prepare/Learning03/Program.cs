34fa9b15cc7d87c270d72e9686a22fb95dae7836
Mauricio Reyes <maureyes.dev@gmail.com>
2024-10-02 10:52:11 -0600

    feat: add learning act 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,22 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new();
+        Fraction fraction2 = new(5);
+        Fraction fraction3 = new(3, 4);
+        Fraction fraction4 = new(1, 3);
+
+        Console.WriteLine($"fraction 1: {fraction1.GetFractionString()}");
+        Console.WriteLine($"fraction 1: {fraction1.GetDecimalFraction()}");
+
+        Console.WriteLine($"fraction 2: {fraction2.GetFractionString()}");
+        Console.WriteLine($"fraction 2: {fraction2.GetDecimalFraction()}");
+
+        Console.WriteLine($"fraction 3: {fraction3.GetFractionString()}");
+        Console.WriteLine($"fraction 3: {fraction3.GetDecimalFraction()}");
+
+        Console.WriteLine($"fraction 4: {fraction4.GetFractionString()}");
+        Console.WriteLine($"fraction 4: {fraction4.GetDecimalFraction()}");
     }
 }
